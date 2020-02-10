239aec7724aa21742ba3ad0c6209e78ac4b4925e
Murutu <murutupeter@email.com>
2020-02-10 11:54:18 +0300

    Created email configurations in config.py

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ class Config:
     debug = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peter:ozil@localhost/laca'
+    
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    
