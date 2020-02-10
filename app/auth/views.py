8bc19e0fbc696dd1ce7ac18454daceb568e14e17
Murutu <murutupeter@email.com>
2020-02-10 15:22:14 +0300

    Created mail_message in views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,11 @@
 from ..email import mail_message
 
 @auth.route('/register',methods = ["GET","POST"])
+def register():
+    from = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.user.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        
+        mail_message
