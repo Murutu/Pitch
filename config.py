d944e03b838a9ec56ba0dc039650db1fe0b0fd31
Murutu <murutupeter@email.com>
2020-02-10 11:47:46 +0300

    Created SQLALCHEMY_DATABASE_URI in config.py

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 import os
 class Config:
+    '''
+    Parent configuration class
+    '''
+    debug = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peter:ozil@localhost/laca'
