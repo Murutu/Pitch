7d6882aa7c89eefe33cea5a7ac35dfec829a7005
Murutu <murutupeter@email.com>
2020-02-12 08:57:28 +0300

    Created errorhandler in errors.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,10 @@ db =SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
