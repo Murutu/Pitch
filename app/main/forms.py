5c0a8589740ebde7c2656ca46ee5597baa335dc7
Murutu <murutupeter@email.com>
2020-02-12 08:48:56 +0300

    Created def new_pitch in views.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    description = TextAreaField("What would you like to pitch?",validators=[Required()])
+	category = RadioField('Label', choices=[ ('promotionpitch','promotionpitch'), ('interviewpitch','interviewpitch'),('pickuplines','pickuplines'),('productpitch','productpitch')],validators=[Required()])
+    submit = SubmitField('Submit')
