bc49d1774e5879152afe75f852a3c6958742417b
Murutu <murutupeter@email.com>
2020-02-15 14:06:44 +0300

    made changes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,9 @@
-from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user,logout_user,login_required
-from .import auth
+from flask import render_template, redirect, url_for, flash, request
+from . import auth
 from ..models import User
-from .forms import LoginForm,RegistrationForm
-from ..import db
-from ..email import mail_message
+from .forms import RegistrationForm, LoginForm
+from .. import db
+from flask_login import login_user, login_required, logout_user
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -14,31 +13,25 @@ def login():
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-        else:
+
         flash('Invalid username or Password')
 
-    title = "pitch login"
+    title = "Pitch Login"
     return render_template('auth/login.html',login_form = login_form,title=title)
-
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
-    from = RegistrationForm()
+    form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.user.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        
-        mail_message("Welcome to pitch","email/welcome",user.email,user=user)
-        
- return render_template('auth/register.html',registration_form = form)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
-
-@auth.route('/about')
-def about():
-    return render_template('auth/about.html')        
